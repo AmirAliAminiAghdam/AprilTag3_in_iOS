42e7a1c67c9df2449fa3c614a8c65e1f7ab15188
Amir Ali <a.aminiaghdam@gmail.com>
2024-12-15 16:40:41 +0100

    Initial Commit

---
+++ AprilTagDemo/AprilTagDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AprilTagDemoApp.swift
+//  AprilTagDemo
+//
+//  Created by amirali amini on 15.12.24.
+//
+
+import SwiftUI
+
+@main
+struct AprilTagDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
